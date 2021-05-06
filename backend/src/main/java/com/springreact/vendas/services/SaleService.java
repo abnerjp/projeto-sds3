2ee7f987de424bd995174040d41470b95e97c318
abnerjp <abner.pelisser@gmail.com>
2021-05-05 23:15:50 -0300

    Pagination

---
+++ backend/src/main/java/com/springreact/vendas/services/SaleService.java
@@ -0,0 +1,30 @@
+package com.springreact.vendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springreact.vendas.dto.SaleDTO;
+import com.springreact.vendas.entities.Sale;
+import com.springreact.vendas.repositories.SaleRepository;
+import com.springreact.vendas.repositories.SellerRepository;
+
+@Service
+
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
